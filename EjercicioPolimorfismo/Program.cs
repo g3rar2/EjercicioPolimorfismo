576153edb5f0c569827ff405bcfd2f7b3779d950
Gerardo Perez <113952421+g3rar2@users.noreply.github.com>
2025-02-09 15:14:19 -0600

    added polimosfirmo by listas

--- EjercicioPolimorfismo/Program.cs
+++ EjercicioPolimorfismo/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using EjercicioPolimorfismo;
+
+List<Guerrero> gerrero=new List<Guerrero>();
+Guerrero guerrero2 = new Guerrero();
+string opcion; 
+
+do
+{
+    Console.Write("Ingrese el rango del guerrero ");
+    guerrero2.categoria = Console.ReadLine();
+
+    Console.Write("Ingrese la estrella del guerrero ");
+    guerrero2.estrella= Console.ReadLine();
+
+    gerrero.Add(guerrero2);
+
+
+    Console.Write("Desea Continuar (S/N) o (s/n) ");
+    opcion = Console.ReadLine();
+
+
+
+} while (opcion.ToUpper() != "N");
+
+foreach (Guerrero egerrero in gerrero)
+{
+    egerrero.Imprimir();
+}
